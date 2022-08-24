7ea58186c6760b893bb4288d7db8bc4fc7bf941c
Svetlana <brodlik@me.com>
2022-08-24 12:46:30 +0300

    Программа находит сумму натуральных элементов от M до  N

---
+++ Task066/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите начальное число M:");
+int minNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число M:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void SumToN (int minNumber, int number, int sum)
+{
+    if (number<minNumber) 
+    {
+        Console.WriteLine($"Сумма элементов в промежутке от M до N равна {sum}"); 
+        return;
+    }
+    sum = sum + (minNumber++);
+    SumToN(minNumber, number, sum);
+}
+
+SumToN(minNumber, number, 0);
